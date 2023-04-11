f092a4ab03a4782357ac83feaa9ef8c194b484da
Samiur Khan <samiurkh1n@gmail.com>
2023-04-11 12:54:55 -0400

    Bug fix: bad test. Color stores floating point of 0 to 1. This gets multiplied by 255 and casted to an int to get a value from 0 to 255 to represent the color in ppm

--- color_test.cpp
+++ color_test.cpp
@@ -7,9 +7,9 @@
 int main() {
   std::stringstream s;
   Color black(0, 0, 0);
-  Color white(255, 255, 255);
+  Color white(1, 1, 1);
   WriteColor(s, black);
   WriteColor(s, white);
-  assert(s.str() == "0 0 0\n65279 65279 65279\n");
+  assert(s.str() == "0 0 0\n255 255 255\n");
   return 0;
 }
